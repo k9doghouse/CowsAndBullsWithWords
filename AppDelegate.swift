e14ad7216f546897e68003f5cb54014d1072f848
AB Murphy <k9doghouse@outlook.com>
2018-10-05 19:16:39 -0500

    More uploading.
    It works.  Just basic.

---
+++ AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CowsAndBulls
+//
+//  Created by k9doghouse 10/02/2018
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
